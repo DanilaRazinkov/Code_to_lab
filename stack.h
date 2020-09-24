4ce2bba045bcd29101917784f7209eab0368adb0
DanilaRazinkov <danilarazinkov@gmail.com>
2020-09-24 18:59:55 +0300

    Стек и оцередь

---
+++ stack.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+class Stack
+{
+	struct node
+	{
+		int value;
+		node* next;
+	};
+private:
+	node *begin;
+public:
+	Stack();
+	int next();
+	void create();
+	void show();
+	void add(int);
+	void delete_el();
+	void clear();
+};
